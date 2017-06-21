d0e1c4be019ffc4c44a61d028ff2b74822403767
Hannah Schuster <hschuster9@gmail.com>
2017-06-20 21:22:07 -0400

    created edit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,4 +5,14 @@ class MoviesController < ApplicationController
   end
 
 
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update(movie_params)
+    redirect_to movie_path
+  end
+
 end
