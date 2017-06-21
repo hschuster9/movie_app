80904fc20b8e1e76ffd0b874925ccffcbbce151a
Hannah Schuster <hschuster9@gmail.com>
2017-06-20 20:07:27 -0400

    created schema, route

---
+++ db/migrate/20170621000429_create_movies.rb
@@ -0,0 +1,12 @@
+class CreateMovies < ActiveRecord::Migration[5.0]
+  def change
+    create_table :movies do |t|
+      t.string :title, null: false
+      t.text :storyline
+      t.date :release_date
+      t.string :genres
+      t.string :link
+  
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Movie.destroy_all
+
+
+spiderman = Movie.create!(title: "Spiderman", storyline: "When bitten by a genetically modified spider, a nerdy, shy, and awkward high school student gains spider-like abilities that he eventually must use to fight evil as a superhero after tragedy befalls his family.", release_date: "05-03-2002", genres: "Action, Adventure", link: "http://www.imdb.com/title/tt0145487/?ref_=fn_al_tt_1")
