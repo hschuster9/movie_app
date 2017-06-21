3c5e5b27c82ee8e0a7ef6ed8f6f2d93f3c3b8474
Hannah Schuster <hschuster9@gmail.com>
2017-06-21 17:00:37 -0400

    search functionality and styling

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,6 +2,11 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
+    if params[:search]
+    @movies = Movie.search(params[:search])
+  else
+    @movies = Movie.all
+  end
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ApplicationRecord
-  
+
+  def self.search(search)
+      where("title ILIKE ? OR storyline ILIKE ? OR genres ILIKE ?", "%#{search}%", "%#{search}%","%#{search}%")
+    end
+
 end
