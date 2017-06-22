47c8d6619fcb33ca4972aa1fc70317a9065e84b7
Hannah Schuster <hschuster9@gmail.com>
2017-06-21 21:42:17 -0400

    added validations for all fields

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ApplicationRecord
+  validates :title, :storyline, :release_date, :genres, :movie_url, :image_url, {presence: true}
   def self.search(search)
     where("title ILIKE ? OR storyline ILIKE ? OR genres ILIKE ?", "%#{search}%", "%#{search}%","%#{search}%")
   end
